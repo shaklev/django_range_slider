5014761b5345c19eef9d5ff40e39d96bef65f274
lcadc17 <lcadc17@localhost.localdomain>
2016-12-15 02:59:26 +0100

    Updated example app - removed static files

--- test_slider/test_slider/settings.py
+++ test_slider/test_slider/settings.py
@@ -119,7 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-STATIC_URL = '/static/'
